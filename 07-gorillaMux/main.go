3d8549cf6f27a5c5c20f1084eec5a4857641ce57
= <=>
2020-12-08 14:56:26 +0000

    get and post methods for the gorilla mux example

--- 07-gorillaMux/main.go
+++ 07-gorillaMux/main.go
@@ -1,5 +1,78 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"time"
 
+	"github.com/gorilla/mux"
+)
+
+type Note struct {
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedOn   time.Time `json:"created_on"`
+}
+
+// store for the Notes collection
+var noteStore = make(map[string]Note)
+
+var id int = 0
+
+// HTTP Post - /api/notes
+func PostNoteHandler(w http.ResponseWriter, req *http.Request) {
+	var note Note
+
+	// decode the incoming json
+	err := json.NewDecoder(req.Body).Decode(&note)
+	if err != nil {
+		panic(err)
+	}
+
+	note.CreatedOn = time.Now()
+	id++
+	k := strconv.Itoa(id)
+	noteStore[k] = note
+
+	j, err := json.Marshal(note)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(j)
+}
+
+// HTTP Get - /api/notes
+func GetNoteHandler(w http.ResponseWriter, req *http.Request) {
+	var notes []Note
+
+	for _, v := range noteStore {
+		notes = append(notes, v)
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	j, err := json.Marshal(notes)
+	if err != nil {
+		panic(err)
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(j)
+}
+
+func main() {
+	r := mux.NewRouter().StrictSlash(false)
+	r.HandleFunc("/api/notes", GetNoteHandler).Methods("GET")
+	r.HandleFunc("/api/notes", PostNoteHandler).Methods("POST")
+	r.HandleFunc("/api/notes/{id}", PutNoteHandler).Methods("PUT")
+	r.HandleFunc("/api/notes/{id}", DeleteNoteHandler).Methods("DELETE")
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	server.ListenAndServe()
 }
